9f27b99c4daa0bc3c45dd2707a4b9acd33693f3f
tparsell-admin <tparsell@berkeley.edu>
2025-12-23 16:27:26 -0800

    tubmas card

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".letter-container").addEventListener("mouseout", () => {
+  document.querySelector(".seal-flap").classList.add("close-seal")
+  document.querySelector(".paper").classList.add("letter-in")
+})
+
